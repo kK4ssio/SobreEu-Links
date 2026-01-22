491a4212b9f520c7505c82b08f502cfec34b0a08
kK4ssio <diasmonteirokassio@gmail.com>
2026-01-22 02:01:07 -0300

    22-01-2026

---
+++ script.js
@@ -0,0 +1,12 @@
+function git(){
+    window.location.href = "https://github.com/kK4ssio";
+}
+
+function linkedin(){
+    window.location.href = "https://www.linkedin.com/in/kassio-dias-monteiro";
+}
+
+function instagram(){
+    window.location.href = "https://www.instagram.com/kaasin_7/";
+    
+}
